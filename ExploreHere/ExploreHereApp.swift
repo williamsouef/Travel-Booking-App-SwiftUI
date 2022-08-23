3d9413fc33eb459627eb1f93a49aaf6d9c6f3bc4
WILLIAM SOUEF <williamsouef@gmail.com>
2022-08-23 16:26:03 +0200

    Initial Commit

---
+++ ExploreHere/ExploreHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreHereApp.swift
+//  ExploreHere
+//
+//  Created by William Souef on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct ExploreHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
