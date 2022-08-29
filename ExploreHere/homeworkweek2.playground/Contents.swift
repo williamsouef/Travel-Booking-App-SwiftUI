fa680412c74277f1de75e3549599534f9cd8c9d1
WILLIAM SOUEF <williamsouef@gmail.com>
2022-08-29 07:17:35 +0200

    adding the playground for the second week of the homework

---
+++ ExploreHere/homeworkweek2.playground/Contents.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+
+struct Hotels  {
+   
+
+    var name : String
+    var description: String
+    var image : String
+    var suitRoom : String?
+    var price : Int
+    
+}
+
+
+
+var hotel1 = Hotels(name: "Negresco", description: "Majestic hotels in Nice", image: "", suitRoom: "yes",price: 250)
+var hotel2 = Hotels(name: "Las Vegas", description: "Luxury Hotel Up to the casino", image: "", suitRoom: "yes",price: 200)
+var hotel3 = Hotels(name: "Tokyo", description: "Minimalist Hotel ", image: "", suitRoom: nil,price: 100)
+var hotel4 = Hotels(name: "Sydney", description: "Wonderful Hotel", image: "", suitRoom: "Yes",price: 200)
+var hotel5 = Hotels(name: "Kenya", description: "Piece of paradise Hotel ", image: "", suitRoom: "Yes",price: 150)
+var hotel6 = Hotels(name: "Bali", description: "Surrended by nature", image: "", suitRoom: nil,price: 80)
+
+
+var hotelList : [Hotels] = []
+
+
+func addItems (newItem: Hotels ) {
+   
+    hotelList.append(newItem)
+}
+addItems(newItem: hotel1)
+addItems(newItem: hotel2)
+addItems(newItem: hotel3)
+addItems(newItem: hotel4)
+addItems(newItem: hotel5)
+addItems(newItem: hotel6)
+
+
+
+func displayItemList ( itemList: [Hotels]) {
+    var itemList = hotelList
+    for itemList in itemList {
+        print(hotel1,hotel2,hotel4,hotel5)
+   
+    }
+    
+}
+
+
+//if hotel6.suitRoom != nil {
+    //print(hotel6.suitRoom!)
+//}
+//if hotel6.suitRoom == nil {
+    
+//}
+//print(hotel1)
+//hotel.forEach() { print($0) }
+
+
+
